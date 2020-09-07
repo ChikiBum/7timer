79d332995d12f956fab96e8df27d4e8305a0c7ad
Alexander Bozhko <obozhko82@gmail.com>
2020-09-07 22:40:33 +0300

    lesson 23 init

--- scripts/script.js
+++ scripts/script.js
@@ -297,4 +297,54 @@ window.addEventListener('DOMContentLoaded',function(){
    };
 
    slider();
+
+   //change image 'Наша команда'
+
+   const changeImage = () => {
+    const command = document.getElementById('command'),
+        commandPhoto = document.querySelectorAll('.command__photo');
+
+    command.addEventListener('mouseover', (event) =>{
+        const target = event.target,
+            origImage = event.target.src;
+ 
+        commandPhoto.forEach((element) => {
+          
+            if (target === element) {
+               event.target.src = target.dataset.img;
+               target.dataset.img = origImage.replace(/http\:\/\/127\.0\.0\.1\:5500\//, '');
+            }
+        });
+    });
+
+    command.addEventListener('mouseout', (event) =>{
+        const target = event.target,
+            origImage = event.target.src;
+        
+        commandPhoto.forEach((element) => {
+                     if (target === element) {
+               event.target.src = target.dataset.img;
+               target.dataset.img = origImage.replace(/http\:\/\/127\.0\.0\.1\:5500\//, '');
+            }
+        });
+    });
+
+   };
+
+   changeImage();
+
+   //calc limit
+   const calcEnterLimit = () => {
+        const calcItem = document.querySelectorAll('input.calc-item');
+
+            calcItem.forEach((elem, index) => {
+                elem.addEventListener('input', () => {
+
+                elem.value = elem.value.replace(/[^0-9]/gi, '');
+
+            });
+        });
+   };
+
+   calcEnterLimit();
 });
