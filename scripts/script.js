5db4fc65bdab0710cc0184829f1f39d2d722ba94
Alexander Bozhko <obozhko82@gmail.com>
2020-09-10 01:07:30 +0300

    Leesson 25 init

--- scripts/script.js
+++ scripts/script.js
@@ -33,7 +33,7 @@ window.addEventListener('DOMContentLoaded',function(){
             timerMinutes.textContent = addFistNum(timer.minutes);
             timerSeconds.textContent = addFistNum(timer.seconds);
            
-            setInterval(updateClock, 1000)
+            setTimeout(updateClock, 1000)
             } else {
                 timerHours.textContent = '00';
                 timerMinutes.textContent = '00';
@@ -348,58 +348,77 @@ window.addEventListener('DOMContentLoaded',function(){
 
    calcEnterLimit();
 
-   //calculator
+    //calculator
+
    const calc = (price = 100) => {
-       const calcBlock = document.querySelector('.calc-block'),
-            calcType = document.querySelector('.calc-type'),
-            calcSquare= document.querySelector('.calc-square'),
-            calcDay = document.querySelector('.calc-day'),
-            calcCount = document.querySelector('.calc-count'),
-            totalValue = document.getElementById('total');
+    const calcBlock = document.querySelector('.calc-block'),
+         calcType = document.querySelector('.calc-type'),
+         calcSquare= document.querySelector('.calc-square'),
+         calcDay = document.querySelector('.calc-day'),
+         calcCount = document.querySelector('.calc-count'),
+         totalValue = document.getElementById('total');
 
-            const countSum = () => {
-                let total = 0,  
-                    countValue = 1,
-                    dayValue = 1;
-                const typeValue = calcType.options[calcType.selectedIndex].value,
-                     squareValue = +calcSquare.value;
+         const countSum = () => {
+             let total = 0,  
+                 countValue = 1,
+                 dayValue = 1;
+             const typeValue = calcType.options[calcType.selectedIndex].value,
+                  squareValue = +calcSquare.value;
 
-                if (calcCount.value > 1){
-                    countValue += (calcCount.value - 1) / 10;
-                }
+             if (calcCount.value > 1){
+                 countValue += (calcCount.value - 1) / 10;
+             }
 
-                if (calcDay.value && calcDay.value < 5){
-                    dayValue *= 2;
-                } else if (calcDay.value && calcDay.value < 10){
-                    dayValue *= 1.5;
-                }
+             if (calcDay.value && calcDay.value < 5){
+                 dayValue *= 2;
+             } else if (calcDay.value && calcDay.value < 10){
+                 dayValue *= 1.5;
+             }
 
-                if (typeValue && squareValue){
-                    total = price * typeValue * squareValue * countValue * dayValue;
-                } 
-            
-                totalValue.textContent = total;
-            };
+             if (typeValue && squareValue){
+                 total = price * typeValue * squareValue * countValue * dayValue;
+             } 
+         
+            let n = 0;
 
-            calcBlock.addEventListener('change', (event) => {
-                const target = event.target;
+            if (total > 0){
+                const animateTotal = setInterval(() => {
+                // console.log(total)
+                    n += 55;
+                    if (n >= total){
+                        totalValue.textContent = total;
+                        clearInterval(animateTotal);
+                        return;
+                    }
+                    totalValue.textContent = n;
 
-                // if (target.matches('.calc-type') || target.matches('.calc-square') ||
-                //     target.matches('.calc-day') || target.matches('.calc-count') )  {
-                //         console.log(target)
-                //     }
+                }, 1);
+            }
+            //  totalValue.textContent = total;
+         };
 
-                // if (target === calcType || target === calcSquare ||
-                //     target === calcDay || target === calcCount ) {
-                //         console.log(target)
-                //     }
+         calcBlock.addEventListener('change', (event) => {
+             const target = event.target;
 
-                if (target.matches('select') || target.matches('input')){
-                   countSum();
-                }
-            });
+             // if (target.matches('.calc-type') || target.matches('.calc-square') ||
+             //     target.matches('.calc-day') || target.matches('.calc-count') )  {
+             //         console.log(target)
+             //     }
 
-   };
+             // if (target === calcType || target === calcSquare ||
+             //     target === calcDay || target === calcCount ) {
+             //         console.log(target)
+             //     }
 
-   calc(100);
+             if (target.matches('select') || target.matches('input')){
+                countSum();
+             }
+         });
+
+    };
+
+    calc(100);
+
+
+
 });
