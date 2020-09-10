aa878576e6ef638c60fdd8d59b3ad84e6d57deea
Alexander Bozhko <obozhko82@gmail.com>
2020-09-11 00:34:13 +0300

    Add validation form

--- scripts/script.js
+++ scripts/script.js
@@ -494,4 +494,29 @@ window.addEventListener('DOMContentLoaded',function(){
 
     sendForm();
 
+    //validate form 
+
+    const validateForm = () => {
+        const phoneFroms = document.querySelectorAll('.form-phone'),
+                form2name = document.getElementById('form2name'),
+                form2message = document.getElementById('form2message');
+
+            phoneFroms.forEach((elem) => {
+                elem.addEventListener('input', () =>{
+                             
+                    elem.value = elem.value.replace(/[^\+\d]/g, '');
+                });
+            });
+
+            form2name.addEventListener('input', () =>{
+            form2name.value = form2name.value.replace(/[^а-яё ]/gi, '');
+            });
+            
+            form2message.addEventListener('input', () =>{
+                form2message.value = form2message.value.replace(/[^а-яё ]/gi, '');
+                });
+    };
+
+    validateForm();
+
  });
